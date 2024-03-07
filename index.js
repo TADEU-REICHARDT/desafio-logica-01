5f98d12dcb0dfff74458dbd3b00b96187af7f81c
Tadeu Reichardt <125933357+TADEU-REICHARDT@users.noreply.github.com>
2024-03-07 11:48:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-console.log("inicio")
-console.log("teste atualização")
+let nickName = ("Debugnator");  
+let myLevel = 3999;
+
+if (myLevel <=1000){ 
+  myLevel = "Ferro"
+}
+else if (myLevel 1001<=2000){ 
+  myLevel = "Bronze"  
+}
+else if (myLevel 2001<=5000){ 
+  myLevel = "Prata" 
+}
+else if (myLevel 5001<=7000){ 
+  myLevel = "ouro" 
+}
+else if (myLevel 7001<=8000){ 
+  myLevel = "Platina" 
+}
+else if (myLevel 8001<=9000){ 
+  myLevel = "kratos"
+}
+else if (myLevel 9001<=10000){ 
+  myLevel = "Keano Rives)"
+}
+else if (myLevel > 10000){ 
+  myLevel = "ChuckNoris"
+}
+
+console.log("O heroi " + nickName + " está no nivel" + myLevel)
