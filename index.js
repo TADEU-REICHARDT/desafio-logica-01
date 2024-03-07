f4b135f8635486ae68e403128596148ed75d4864
Tadeu Reichardt <125933357+TADEU-REICHARDT@users.noreply.github.com>
2024-03-07 15:53:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 let nickName = ("Debugnator");  
-let myLevel = 3999;
+let myLevel = 10500;
 
-if (myLevel <=1000){ 
-  myLevel = "Ferro"
-}
-else if (myLevel 1001<=2000){ 
-  myLevel = "Bronze"  
-}
-else if (myLevel 2001<=5000){ 
+if(myLevel <= 1000) { 
+ myLevel = "Ferro"
+} 
+else if ((myLevel >= 1001) && (myLevel <= 2000)) { 
+ myLevel = "Bronze"  
+} 
+else if ((myLevel >= 2001) && (myLevel <= 5000)) { 
   myLevel = "Prata" 
-}
-else if (myLevel 5001<=7000){ 
-  myLevel = "ouro" 
-}
-else if (myLevel 7001<=8000){ 
+} 
+else if ((myLevel >= 5001) && (myLevel <= 7000)) { 
+  myLevel = "ouro"  
+} 
+else if ((myLevel >= 7001) && (myLevel <= 8000)) { 
   myLevel = "Platina" 
-}
-else if (myLevel 8001<=9000){ 
+} 
+else if ((myLevel >= 8001) && (myLevel <= 9000)) { 
   myLevel = "kratos"
-}
-else if (myLevel 9001<=10000){ 
-  myLevel = "Keano Rives)"
-}
-else if (myLevel > 10000){ 
-  myLevel = "ChuckNoris"
-}
+} 
+else if ((myLevel >= 9001) && (myLevel <= 10000)) { 
+  myLevel = "Keano Rives"
+} 
+else (myLevel = "ChuckNoris") 
 
-console.log("O heroi " + nickName + " está no nivel" + myLevel)
+console.log("O heroi " + nickName + " está no nivel " + myLevel)
